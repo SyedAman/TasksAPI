b66edfb6608d7d2ac482d632797e7f9d50e6c068
Syed Aman <syednashikaman@gmail.com>
2024-03-05 18:28:48 -0500

    Fix incorrect routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
-namespace :api do
-  resources :tasks do
-    member do
-      post 'assign'
-      put 'progress'
+Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    resources :tasks do
+      member do
+        post 'assign'
+        put 'progress'
+      end
     end
+    resources :users do
+      resources :tasks, only: [:index]
+    end
+    get 'tasks/overdue', to: 'tasks#overdue'
+    get 'tasks/status/:status', to: 'tasks#by_status'
+    get 'tasks/completed', to: 'tasks#completed'
+    get 'tasks/statistics', to: 'tasks#statistics'
   end
-  resources :users do
-    resources :tasks, only: [:index]
-  end
-  get 'tasks/overdue', to: 'tasks#overdue'
-  get 'tasks/status/:status', to: 'tasks#by_status'
-  get 'tasks/completed', to: 'tasks#completed'
-  get 'tasks/statistics', to: 'tasks#statistics'
 end
