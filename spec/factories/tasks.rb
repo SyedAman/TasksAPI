42c0dfbc3648f053abee87b1aa0f917e2ccaa641
Syed Aman <syednashikaman@gmail.com>
2024-03-06 10:33:03 -0500

    Updat ethe factory bot to be a bit more accurate

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :task do
     title { "MyString" }
     description { "MyText" }
-    due_date { "2024-03-05 16:20:33" }
-    status { 1 }
-    priority { 1 }
-    completed_date { "2024-03-05 16:20:33" }
+    due_date { Time.now + 1.week }
+    status { :in_progress }
+    priority { :medium }
+    completed_date { nil }
   end
 end
