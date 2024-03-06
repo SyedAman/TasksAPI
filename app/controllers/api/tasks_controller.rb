44f45ebfab519ccf5841468c7b4252d4f4440c35
Syed Aman <syednashikaman@gmail.com>
2024-03-06 12:45:49 -0500

    Pass 1 more test

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -3,7 +3,7 @@ module Api
     before_action :set_task, only: %i[update destroy assign progress]
 
     def create
-      task = Task.new(task_params.merge(user_id: params[:task][:user_id]))
+      task = Task.new(task_params.merge(user_id: params.dig(:task, :user_id)))
       if task.save
         render json: task, status: :created
       else
@@ -13,7 +13,7 @@ module Api
 
     def update
       if @task.update(task_params)
-        @task.completed_date = Time.current if task_params[:status] == 'completed'
+        @task.completed_date = Time.current if @task.status == 'completed' && task_params[:status] == 'completed'
         @task.save
         render json: @task
       else
@@ -27,14 +27,15 @@ module Api
     end
 
     def assign
-      user = User.find(params[:user_id]) # ensure you pass the correct parameter from the test
-      @task.update(user: user)
+      # Assign a user to a task
+      user = User.find(params[:user_id])
+      @task.update(user_id: user.id)
       render json: @task
     end
 
     def progress
-      if params[:progress].to_i.between?(0, 100) # Ensuring progress is between 0 and 100
-        @task.update(progress: params[:progress])
+      if params[:task][:progress].to_i.between?(0, 100) # Ensuring progress is between 0 and 100
+        @task.update(progress: params[:task][:progress])
         render json: @task
       else
         render json: { error: 'Progress must be between 0 and 100' }, status: :unprocessable_entity
@@ -50,6 +51,5 @@ module Api
     def task_params
       params.require(:task).permit(:title, :description, :due_date, :status, :priority)
     end
-
   end
 end
